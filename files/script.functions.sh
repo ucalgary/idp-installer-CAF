fb889b70d9b2cafd33277ee6b29fe6b4c7ccb154
Cosmin Dinu <cosmin.dinu@cybera.ca>
2015-02-17 14:35:45 -0700

    fix metadata-providers.xml 2

--- files/script.functions.sh
+++ files/script.functions.sh
@@ -1288,6 +1288,7 @@ patchShibbolethConfigs ()
         mv /opt/shibboleth-idp/conf/attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml.dist
         cp ${Spath}/files/${my_ctl_federation}/attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml
         patch /opt/shibboleth-idp/conf/handler.xml -i ${Spath}/${prep}/handler.xml.diff >> ${statusFile} 2>&1
+	dos2unix /opt/shibboleth-idp/conf/metadata-providers.xml
         patch /opt/shibboleth-idp/conf/metadata-providers.xml -i ${Spath}/xml/${my_ctl_federation}/metadata-providers.xml.diff
         cp ${Spath}/xml/${my_ctl_federation}/attribute-resolver.xml /opt/shibboleth-idp/conf/attribute-resolver.xml
 
