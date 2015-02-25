ddbb34ce8ed97c0e94683887c0e9ab56e7864f2d
Cosmin Dinu <cosmin.dinu@cybera.ca>
2015-02-25 15:03:24 -0700

    fix placeholder

--- files/CAF/script.override.functions.sh
+++ files/CAF/script.override.functions.sh
@@ -205,12 +205,12 @@ patchShibbolethConfigs ()
 		repStr='<!-- EPTID PRINCIPAL CONNECTOR PLACEHOLDER -->'
 		sed -i -e "/^${repStr}$/r ${Spath}/xml/${my_ctl_federation}/eptid.add.princCon" -e "/^${repStr}$/d" /opt/shibboleth-idp/conf/attribute-resolver.xml
 
-                repStr='<!-- LDAP CONNECTOR PLACEHOLDER -->'
-                sed -i -e "/^${repStr}$/r ${Spath}/xml/${my_ctl_federation}/ldapconn.txt" -e "/^${repStr}$/d" /opt/shibboleth-idp/conf/attribute-resolver.xml
-
 		repStr='<!-- EPTID FILTER PLACEHOLDER -->'
 		sed -i -e "/^${repStr}$/r ${Spath}/xml/${my_ctl_federation}/eptid.add.filter" -e "/^${repStr}$/d" /opt/shibboleth-idp/conf/attribute-filter.xml
 	fi
+
+        repStr='<!-- LDAP CONNECTOR PLACEHOLDER -->'
+        sed -i -e "/^${repStr}$/r ${Spath}/xml/${my_ctl_federation}/ldapconn.txt" -e "/^${repStr}$/d" /opt/shibboleth-idp/conf/attribute-resolver.xml
 
 	echo "applying chown "
 	chmod o+r /opt/shibboleth-idp/conf/attribute-filter.xml
