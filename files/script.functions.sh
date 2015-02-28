81d4cbfdaded47d1a63ee5ab3c3cb69c88c01bb1
Cosmin Dinu <cosmin.dinu@cybera.ca>
2015-02-27 20:11:22 -0700

    ldapUrl

--- files/script.functions.sh
+++ files/script.functions.sh
@@ -922,7 +922,7 @@ EOM
 	# Set LDAP configuration (needed for both cas and ldap)
         cat << EOM > ldap.properties.tmp
 idp.authn.LDAP.authenticator                    = ${ldapAuthenticator}
-idp.authn.LDAP.ldapURL                          = ldap://${ldapserver}
+idp.authn.LDAP.ldapURL                          = ${ldapUrl}
 idp.authn.LDAP.useStartTLS                      = ${ldapStartTLS}
 idp.authn.LDAP.useSSL                           = ${ldapSSL}
 idp.authn.LDAP.sslConfig                        = certificateTrust
