cfa75e074f9fbc649b1ee7c906f6ee2be848f525
Chris Phillips <chris.phillips@canarie.ca>
2015-06-29 15:55:39 -0400

    netstat bootstrap

--- files/script.functions.sh
+++ files/script.functions.sh
@@ -385,6 +385,8 @@ setHostnames() {
 	Dname=`${Echo} ${FQDN} | cut -d\. -f2-`
 	if [ "${FQDN}" = "Host" ]
 	then
+		eval ${dist_install_netstat} >> ${statusFile} 2>&1
+		
 		myInterface=`netstat -nr | grep "^0.0.0.0" | awk '{print $NF}'`
 		myIP=`ip addr list ${myInterface} | grep "inet " | cut -d' ' -f6 | cut -d/ -f1`
 		Dname=`host -t A ${myIP} | head -1 | awk '{print $NF}' | cut -d\. -f2- | sed 's/\.$//'`
@@ -858,7 +860,7 @@ configShibbolethXMLAttributeResolverForLDAP ()
 	orgTopDomain=`${Echo} ${certCN} | cut -d. -f2-`
 
 	${Echo} "Processing Attribute-resolver.xml customizations"
-	
+
 	cat ${Spath}/xml/${my_ctl_federation}/attribute-resolver.xml.template \
 		| sed -re "s/NiNcRePlAcE/${ninc}/;s/CeRtAcRoNyM/${certAcro}/;s/CeRtOrG/${certOrg}/;s/CeRtC/${certC}/;s/CeRtLoNgC/${certLongC}/" \
 		| sed -re "s/SCHAC_HOME_ORG/${orgTopDomain}/" \
