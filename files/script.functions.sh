c5155c728c739b7567250ac160b3b49efb0a6416
Anders Lördal <anders@lordal.se>
2016-02-19 16:18:55 +0100

    Corrected metadata insertion for google

--- files/script.functions.sh
+++ files/script.functions.sh
@@ -2026,7 +2026,6 @@ patchShibbolethConfigs ()
 		((googleRelayLine--))
 		sed -i "${googleRelayLine}r${Spath}/xml/${my_ctl_federation}/google-relay.diff.template" /opt/shibboleth-idp/conf/relying-party.xml
 		googleMetaLine=`grep -n '</MetadataProvider>' /opt/shibboleth-idp/conf/metadata-providers.xml | tail -n 1 | cut -d: -f1`
-		((googleMetaLine--))
 		sed -i "${googleMetaLine}i <MetadataProvider id=\"GoogleMD\" xsi:type=\"FilesystemMetadataProvider\" metadataFile=\"/opt/shibboleth-idp/metadata/google.xml\" />" /opt/shibboleth-idp/conf/metadata-providers.xml
 
                 cat ${Spath}/xml/${my_ctl_federation}/google.xml | sed -re "s/GoOgLeDoMaIn/${googleDom}/" > /opt/shibboleth-idp/metadata/google.xml
